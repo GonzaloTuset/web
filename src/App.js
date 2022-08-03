1616c2086a472e937b7323f2261c59c9ec3764ec
GonzaloTuset <soracerito@gmail.com>
2022-08-02 23:10:34 -0300

    nuevas formas

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import json from './data.json';
 import{reversa}from'./functions/reverse';
-import{filter}from'./functions/filtroId';
+import{filter,garabato}from'./functions/filtroId';
 import{par}from'./functions/par';
 import{show}from'./functions/show';
+
 //importo la informacion que tenga a el archivo a especificar a un objeto
 
 function App() {  
@@ -17,7 +18,7 @@ function App() {
           <button name="button" onClick={reversa}>reversa</button>
           <button name="button" onClick={par}>filtra por id par</button>
           <button name="button" onClick={filter}>filtra por id de usuario</button>{/*VA A RECIBIR UN NUMERO Y VA A FILTRAR POR ESE NUMERO*/}
-
+          <button name="button" onClick={garabato}>otro filtra por id de usuario</button>
         </p>
 
 
--- src/functions/filtroId.js
+++ src/functions/filtroId.js
@@ -1,7 +1,37 @@
 import json from '../data.json'
 export function filter() {
+   
     var ide = prompt("escribe un número del 1 al 10");
+    var ide2=parseInt(ide);//pasa variable ide que tiene un valor string a uno númerico
+    if(ide2>=1 && ide2<=10  ){
+        console.log(ide2>1 );
+        console.log(ide2<=10);
+        console.log(json['data'].filter(filtro => filtro['userId'] === parseInt(ide)));
+        console.log(true);
+    }
+    else{
+        ide = prompt("escribe un número del 1 al 10");
+        ide2=parseInt(ide);
+        if(ide2>=1 && ide2<=10){
+            console.log(json['data'].filter(filtro => filtro['userId'] === parseInt(ide)));
+        }
+    }
+
     console.log(ide);
-    console.log(json['data'].filter(filtro => filtro['userId'] === parseInt(ide)));
+    
+
+  }
+  export function garabato(){
+    var ide = prompt("escribe un número del 1 al 10");
+    var ide2=parseInt(ide);//pasa variable ide que tiene un valor string a uno númerico
+    if(ide2>=1 && ide2<=10  ){
+        console.log(ide2>1 );
+        console.log(ide2<=10);
+        console.log(json['data'].filter(filtro => filtro['userId'] === parseInt(ide)));
+        console.log(true);
+    }
+    else{
+       garabato();
+    }
 
   }
