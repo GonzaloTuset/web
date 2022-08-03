705bdeba2858232d2d200b5c78765bd7a6bdc035
GonzaloTuset <soracerito@gmail.com>
2022-08-02 22:10:15 -0300

    fixproblem

--- src/App.js
+++ src/App.js
@@ -4,50 +4,51 @@ import json from './data.json';
 //importo la informacion que tenga a el archivo a especificar a un objeto
 
 function App() {
-  function show(){
+  function show() {
     console.log("buenas");
     console.log(json);
-    
+
   }
-  function reversa(){
+  function reversa() {
     json['data'].reverse();
     console.log(json['data']);
     //le pido a el objeto json que de vuelta todo los valores de 'data'.
   }
-    function par(){
-      //priero filtrar
-     console.log(json['data'].filter(objeto /*se crea un parametro que alamcena todo que haya en el array y es lo que luego recibe el filtro*/ => objeto["id"] %2 === /*estrictamente parecido*/ 0 /*hace la operacion logica */ ));
-      //filtrar por id par             /*se crea un parametro que alamcena todo que haya en el array y es lo que luego recibe el filtro*/
-      console.log(json['data'].filter(
-        (objeto)=>{
-          
-          return  objeto["id"] %2 === 0
-        }));
- 
-  }
-function filter(){
-  var ide= prompt ("escribe un muero del 1 al 10");
-  console.log(ide);
-    console.log(json['data'].filter(filtro => filtro['userId'] == ide ));
-    
-    
+  function par() {
+    //priero filtrar
+    console.log(json['data'].filter(objeto /*se crea un parametro que alamcena todo que haya en el array y es lo que luego recibe el filtro*/ => objeto["id"] % 2 === /*estrictamente parecido*/ 0 /*hace la operacion logica */));
+    //filtrar por id par             /*se crea un parametro que alamcena todo que haya en el array y es lo que luego recibe el filtro*/
+    console.log(json['data'].filter(
+      (objeto) => {
+
+        return objeto["id"] % 2 === 0
+      }));
 
   }
-  console.log(0,1*0,1);
-  
+
+  function filter() {
+    var ide = prompt("escribe un número del 1 al 10");
+    console.log(ide);
+    console.log(json['data'].filter(filtro => filtro['userId'] === parseInt(ide)));
+
+
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        
+
         <p>
           <button name="button" onClick={show}>mostrar toda la info</button>
           <button name="button" onClick={reversa}>reversa</button>
           <button name="button" onClick={par}>filtra por id par</button>
           <button name="button" onClick={filter}>filtra por id de usuario</button>{/*VA A RECIBIR UN NUMERO Y VA A FILTRAR POR ESE NUMERO*/}
-          
+
         </p>
-       
-    
+
+
       </header>
     </div>
   );
